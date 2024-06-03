6d170985691f41b41e5fee80c60219c6338ed1b2
Pixelsuft <yuskivl07@mail.ru>
2024-06-03 12:09:05 +0700

    wip

--- include/ui-mobile.h
+++ include/ui-mobile.h
@@ -10,9 +10,17 @@ typedef struct {
     void (*on_up)(void* this, SDL_FPoint* pos);
     void (*on_move)(void* this, SDL_FPoint* dt);
     void (*set_rect)(void* this, SDL_FRect* rect);
+    void (*draw)(void* this);
 } mobui_elem;
 
+typedef struct {
+    mobui_elem base;
+    SDL_Texture* tex;
+    char* text;
+} mobui_button;
+
 void mobui_init_elem(void* elem);
+void mobui_init_button(mobui_button* this);
 void mobui_init(void);
 void mobui_quit(void);
 void mobui_run_main(void);
--- src/ui-mobile.c
+++ src/ui-mobile.c
@@ -10,6 +10,14 @@ static SDL_Window* win = NULL;
 static SDL_Renderer* ren = NULL;
 static TTF_Font* fnt = NULL;
 
+typedef struct {
+    mobui_elem* elems[10];
+    size_t elem_count;
+    mobui_button go_btn;
+} mobui_page;
+
+mobui_page page;
+
 void mobui_init_elem(void* elem) {
     mobui_elem* this = elem;
     this->on_down = NULL;
@@ -17,6 +25,10 @@ void mobui_init_elem(void* elem) {
     this->on_move = NULL;
     this->set_rect = NULL;
     this->rect.x = this->rect.y = this->rect.w = this->rect.h = 0.0f;
+}
+
+void mobui_init_button(mobui_button* this) {
+    mobui_init_elem(this);
 }
 
 void mobui_run_main(void) {
@@ -30,6 +42,13 @@ void mobui_run_main(void) {
                     break;
                 }
             }
+        }
+        for (size_t i = 0; i < page.elem_count; i++) {
+            if (page.elems[i] == NULL)
+                continue;
+            if (page.elems[i]->draw == NULL)
+                continue;
+            page.elems[i]->draw(page.elems[i]);
         }
         SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
         SDL_RenderClear(ren);
@@ -45,6 +64,9 @@ void mobui_init(void) {
         return;
     win = display_get_handle(0);
     ren = display_get_handle(1);
+    memset(page.elems, 0, sizeof(page.elems));
+    page.elems[0] = (mobui_elem*)&page.go_btn;
+    page.elem_count = 10;
 }
 
 void mobui_quit(void) {
